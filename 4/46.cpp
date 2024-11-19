4f157f97db15ed76105229294a98ad52359e4ace
J1mmyChanga <epiakovmartynenkovv@yandex.ru>
2024-11-19 19:23:17 +0300

    commit

--- 4/46.cpp
+++ 4/46.cpp
@@ -18,7 +18,7 @@ int converter(char line) {
 }
 
 int main() {
-    int cur, prev;
+    int cur, prev, prev2 = -1, count_of_reps = 1;
     string num, symb;
     cout << "Введите римское число:" << endl;
     cin >> num;
@@ -26,17 +26,42 @@ int main() {
     for (int i = 0; i < num.length(); i++) {
         numbers[i] = converter(num[i]);
     }
-    for (int i = 1; i < num.length(); i++) {
-        prev = numbers[i-1];
-        cur = numbers[i];
-        if (cur > prev and prev != 1) {
+    for (int i = 0; i < num.length(); i++) {
+        if (numbers[i] == numbers[i+2] and numbers[i+1] > numbers[i]) {
             cout << "Такое число существовать не может!";
             return 0;
         }
-        else if (cur > prev and prev == 1) {
+    }
+    for (int i = 1; i < num.length(); i++) {
+        prev = numbers[i-1];
+        cur = numbers[i];
+        if (i >= 2) {
+            prev2 = numbers[i-2];
+            if (cur == prev2 and prev > cur) {
+                cout << "Такое число существовать не может!";
+                return 0;
+            }
+        }
+        int n = cur - prev;
+        if (n == 0) {
+            count_of_reps++;
+            if (cur == 5 or cur == 50 or cur == 500 or count_of_reps > 3) {
+                cout << "Такое число существовать не может!";
+                return 0;
+            }
+        }
+        if (n > 0) {
+            count_of_reps = 1;
+            if ((n != 9 and n != 45 and n != 90 and n != 450 and n != 900) or (prev == prev2)) {
+                cout << "Такое число существовать не может!";
+                return 0;
+            }
             numbers[i-1] = 0;
             numbers[i] = cur - prev;
         }
+        if (n < 0) {
+            count_of_reps = 1;
+        }
     }
-        cout << accumulate(numbers, numbers + num.length(), 0);
+    cout << accumulate(numbers, numbers + num.length(), 0);
 }

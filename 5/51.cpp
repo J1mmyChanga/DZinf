d188230503e568b7578328520147baddb696e27d
J1mmyChanga <epiakovmartynenkovv@yandex.ru>
2024-12-17 01:07:27 +0300

    commit

--- 5/51.cpp
+++ 5/51.cpp
@@ -2,19 +2,21 @@
 using namespace std;
 
 int main() {
-    int a, b;
+    int a, b, a1, b1;
     cout << "Введите два числа, для которых ищется НОД:" << endl;
     cin >> a >> b;
+    a1 = a;
+    b1 = b;
 
-    // while (a != 0 and b != 0) {
-    //     if (a > b) {a = a % b;}
-    //     else {b = b % a;}
-    // }
-    // cout << "НОД (делением):" << a + b;
-
-    while (a != b) {
-        if (a > b) {a = a - b;}
-        else {b = b - a;}
+    while (a != 0 and b != 0) {
+        if (a > b) {a = a % b;}
+        else {b = b % a;}
     }
-    cout << "НОД (вычитанием):" << a;
+    cout << "НОД (делением):" << a + b << endl;
+
+    while (a1 != b1) {
+        if (a1 > b1) {a1 = a1 - b1;}
+        else {b1 = b1 - a1;}
+    }
+    cout << "НОД (вычитанием):" << a1;
 }

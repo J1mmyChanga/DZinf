2d7e64fca4c763eef3b0d9d5f8f375ca4c221c5d
J1mmyChanga <epiakovmartynenkovv@yandex.ru>
2024-12-19 17:48:02 +0300

    commit

---
+++ 5/obrabotky_1(27).cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <string>
+#include <vector>
+using namespace std;
+
+string lower_line(string line) {
+    transform(line.begin(), line.end(), line.begin(), ::tolower);
+    return line;
+}
+
+int main() {
+    char symbol;
+    cout << "Введите символ, количество повторений, которого необходимо найти: " << endl;
+    cin >> symbol;
+
+    ofstream fout("file.txt");
+    fout << "Keith recently came back from a trip to Chicago, Illinois. This midwestern metropolis is found along the shore of Lake Michigan." << endl;
+    fout << "During his visit, Keith spent a lot of time exploring the city to visit important landmarks and monuments." << endl;
+    fout << "Keith loves baseball, and he made sure to take a visit to Wrigley Field. Not only did he take a tour of this spectacular stadium, but he also got to watch a Chicago Cubs game." << endl;
+    fout << "In the stadium, Keith and the other fans cheered for the Cubs. Keith was happy that the Cubs won with a score of 5-4." << endl;
+    fout.close();
+
+    string line;
+    int counter = 0;
+    ifstream fin("file.txt");
+    while (getline(fin, line)) {
+        line = lower_line(line);
+        vector<char> mas(line.begin(), line.end());
+        counter = counter + count(mas.begin(), mas.end(), symbol);
+    }
+    cout << "Количество повторений элементов " << symbol << " в тексте: " << counter;
+    fin.close();
+}
---
+++ 5/obrabotky_2(15).cpp
@@ -0,0 +1,3 @@
+//
+// Created by user on 19.12.2024.
+//
